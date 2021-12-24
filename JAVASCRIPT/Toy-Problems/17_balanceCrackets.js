e0c968a557abc04bbb6a4f8685c5125dc7e56225
sora9z <sssssssorasss@gmail.com>
2021-12-24 23:35:56 +0900

    Toy 17 완료

--- JAVASCRIPT/Toy-Problems/17_balanceCrackets.js
+++ JAVASCRIPT/Toy-Problems/17_balanceCrackets.js
@@ -18,95 +18,98 @@ Advanced
 모든 종류의 괄고 ().{}.[]가 포함된 문자열을 입력받고 짝이 맞는지 여부 return
 */
 
-const balancedBrackets = function (str) {
-  // TODO Data Type Stack을 이용하자!
+// const balancedBrackets1 = function (str) {
+//   // TODO Data Type Stack을 이용하자!
 
-  // str에서 Open Bracket을 str index를 key로 하는 객체의 형태로 전부 받는다
-  // str에서 closed Bracket을 index를 key로 하는 객체의 형태로 전부 받는다.
-  const getBrackObjects = (str) => {
-    let opBracket = {};
-    let closeBracket = {};
+//   // str에서 Open Bracket을 str index를 key로 하는 객체의 형태로 전부 받는다
+//   // str에서 closed Bracket을 index를 key로 하는 객체의 형태로 전부 받는다.
+//   const getBrackObjects = (str) => {
+//     let opBracket = {};
+//     let closeBracket = {};
 
-    for (let i = 0; i < str.length; i++) {
-      // 열린 괄호이면 opBracket에, 아니라면 closeBracket에 저장한다.
-      str[i] === "(" || str[i] === "{" || str[i] === "["
-        ? (opBracket[i] = str[i])
-        : (closeBracket[i] = str[i]);
-    }
-    return [opBracket, closeBracket];
-  };
+//     for (let i = 0; i < str.length; i++) {
+//       // 열린 괄호이면 opBracket에, 아니라면 closeBracket에 저장한다.
+//       str[i] === "(" || str[i] === "{" || str[i] === "["
+//         ? (opBracket[i] = str[i])
+//         : (closeBracket[i] = str[i]);
+//     }
+//     return [opBracket, closeBracket];
+//   };
 
-  let opBrackets = ["(", "[", "{"];
-  let clBrackets = [")", "]", "}"];
+//   let opBrackets = ["(", "[", "{"];
+//   let clBrackets = [")", "]", "}"];
 
-  const [openB, nextB] = getBrackObjects(str);
-  // stack에는 openB를, queue에는 closeB를 한 개씩 넣어서 비교한다.
-  let [stackO, stackC] = [Object.keys(openB), Object.keys(closeB)]; // key정보를 사용하여 open Brack의 index가 closeBrackt보다 먼저 오는지 확인한다.
+//   const [openB, nextB] = getBrackObjects(str);
+//   // stack에는 openB를, queue에는 closeB를 한 개씩 넣어서 비교한다.
+//   let [stackO, stackC] = [Object.keys(openB), Object.keys(closeB)]; // key정보를 사용하여 open Brack의 index가 closeBrackt보다 먼저 오는지 확인한다.
 
-  while (stackO.length !== 0) {
-    let op = stackO.pop();
-    let cl = stackC.pop();
+//   while (stackO.length !== 0) {
+//     let op = stackO.pop();
+//     let cl = stackC.pop();
 
-    // 두 문자가 한 쌍이라면
-    if (opBrackets.indexOf(openB[op]) === clBrackets.indexOf(closeB[cl]))
-      if (op > cl) return false; // 만얃 op보다 cl 기호가 앞에 있다면 false
-  }
-  return true;
-};
+//     // 두 문자가 한 쌍이라면
+//     if (opBrackets.indexOf(openB[op]) === clBrackets.indexOf(closeB[cl]))
+//       if (op > cl) return false; // 만얃 op보다 cl 기호가 앞에 있다면 false
+//   }
+//   return true;
+// };
 
-//! 2 번째
-const balancedBrackets2 = function (str) {
-  // TODO Data Type Stack을 이용하자!
-  let strtoArr = str.split(""); // str을 배열로 변환
+// //! 2 번째
+// const balancedBrackets2 = function (str) {
+//   // TODO Data Type Stack을 이용하자!
+//   let strtoArr = str.split(""); // str을 배열로 변환
 
-  const getOpenBracket = (arr) => {
-    // stack에 open bracket을 넣는 method . stack에 들어갈 문자를 제외한 문자들을 배열로 return 한다.
-    for (let i = 0; i < arr.length; i++) {
-      if (opBracket.includes(arr[i])) stack.push(arr[i]);
-      else return stack.length === 0 ? false : arr.slice(i); // 만약 stack이  비어있다면 return false
-    }
-  };
+//   const getOpenBracket = (arr) => {
+//     // stack에 open bracket을 넣는 method . stack에 들어갈 문자를 제외한 문자들을 배열로 return 한다.
+//     for (let i = 0; i < arr.length; i++) {
+//       if (opBracket.includes(arr[i])) stack.push(arr[i]);
+//       else return stack.length === 0 ? false : arr.slice(i); // 만약 stack이  비어있다면 return false
+//     }
+//   };
 
-  // open 문자와 close 문자를 각 각 배열로 만든다
-  // stack 빈 배열을 선언한다.
-  let opBracket = ["(", "[", "{"];
-  let clBracket = [")", "]", "}"];
-  let stack = [];
+//   // open 문자와 close 문자를 각 각 배열로 만든다
+//   // stack 빈 배열을 선언한다.
+//   let opBracket = ["(", "[", "{"];
+//   let clBracket = [")", "]", "}"];
+//   let stack = [];
 
-  // 입력으로 받은 str에서 ),],} 중 하나가 나올 떄까지 Stck에 넣는다. -> getOpenBracket(str)
-  let remains = getOpenBracket(strtoArr); // stack에 넣은 나머지 문자들을 remain 변수에 배열 자료형으로  초기화
+//   // 입력으로 받은 str에서 ),],} 중 하나가 나올 떄까지 Stck에 넣는다. -> getOpenBracket(str)
+//   let remains = getOpenBracket(strtoArr); // stack에 넣은 나머지 문자들을 remain 변수에 배열 자료형으로  초기화
 
-  // stack의 길이가 0이 될 때까지 while문을 돌린다.
-  while (stack.length !== 0) {
-    nextB = remains[0]; // stack에 넣은 문자의 바로 다음 문자
-    openB = stack[stack.length - 1]; // stack의 제윌 위의 문자
+//   // stack의 길이가 0이 될 때까지 while문을 돌린다.
+//   while (stack.length !== 0) {
+//     nextB = remains[0]; // stack에 넣은 문자의 바로 다음 문자
+//     openB = stack[stack.length - 1]; // stack의 제윌 위의 문자
 
-    // 만약 다음 문자들이 한  쌍이라면
-    if (clBracket.indexOf(nextB) === opBracket.indexOf(openB)) {
-      stack.pop(); // stack에서 제거한다.
-      remains.shift(); // ramains에서 제거한다.
-      if (!stack.length) {
-        // 만약 stack이 비었다면  남아있는 문자에서 stack을 채운다.
-        if (getOpenBracket(remains) === false) return false; // 결과가   false면 return false
-      }
-    }
-    // 만얃 str의 다음 문자가 [,{,( 중에 있으면 (Openbracket 문자이면) stack에 추가한다.
-    else if (opBracket.includes(nextB)) stack.push(nextB);
-    // 위의 두 경우가 아니면 return false
-    else return false;
-  }
+//     // 만약 다음 문자들이 한  쌍이라면
+//     if (clBracket.indexOf(nextB) === opBracket.indexOf(openB)) {
+//       stack.pop(); // stack에서 제거한다.
+//       remains.shift(); // ramains에서 제거한다.
+//       if (!stack.length) {
+//         // 만약 stack이 비었다면  남아있는 문자에서 stack을 채운다.
+//         if (getOpenBracket(remains) === false) return false; // 결과가   false면 return false
+//       }
+//     }
+//     // 만얃 str의 다음 문자가 [,{,( 중에 있으면 (Openbracket 문자이면) stack에 추가한다.
+//     else if (opBracket.includes(nextB)) stack.push(nextB);
+//     // 위의 두 경우가 아니면 return false
+//     else return false;
+//   }
 
-  return true;
-};
+//   return true;
+// };
 
 //! 3 번째 - string으로 진행 해보기
-const balancedBrackets3 = function (str) {
+const balancedBrackets2 = function (str) {
   // TODO Data Type Stack을 이용하자!
+
+  if (str.length === 1) return false;
 
   const getOpenBracket = (str) => {
     // stack에 open bracket을 넣는 method . stack에 들어갈 문자를 제외한 문자들을 배열로 return 한다.
     for (let i = 0; i < str.length; i++) {
-      if (opBracket.includes(str[i])) stack.push(str[i]);
+      if (str[i] === "(" || str[i] === "{" || str[i] === "[")
+        stack.push(str[i]);
       else return stack.length === 0 ? false : str.slice(i); // 만약 stack이  비어있다면 return false
     }
   };
@@ -119,7 +122,7 @@ const balancedBrackets3 = function (str) {
 
   // 입력으로 받은 str에서 ),],} 중 하나가 나올 떄까지 Stck에 넣는다. -> getOpenBracket(str)
   let remains = getOpenBracket(str); // stack에 넣은 나머지 문자들을 remain 변수에 배열 자료형으로  초기화
-
+  if (!remains.length) return false;
   // stack의 길이가 0이 될 때까지 while문을 돌린다.
   while (stack.length !== 0) {
     nextB = remains[0]; // stack에 넣은 문자의 바로 다음 문자
@@ -131,11 +134,13 @@ const balancedBrackets3 = function (str) {
       remains = remains.slice(1); // ramains에서 첫 번째 문자를 제거한다.
       if (!stack.length) {
         // 만약 stack이 비었다면  남아있는 문자에서 stack을 채운다.
-        if (getOpenBracket(remains) === false) return false; // 결과가   false면 return false
+        if (getOpenBracket(remains) === false) return false;
+        // 결과가   false면 return false
+        else remains = remains.slice(1);
       }
     }
     // 만얃 str의 다음 문자가 [,{,( 중에 있으면 (Openbracket 문자이면) stack에 추가한다.
-    else if (opBracket.includes(nextB)) stack.push(nextB);
+    else if (nextB === "(" || nextB === "{" || nextB === "[") stack.push(nextB);
     // 위의 두 경우가 아니면 return false
     else return false;
   }
@@ -143,62 +148,97 @@ const balancedBrackets3 = function (str) {
   return true;
 };
 
-//! 4 번째 - string으로 진행 해보기 + getOpenBracket 빼보기
-
-const balancedBrackets4 = function (str) {
-  // open 문자와 close 문자를 각 각 배열로 만든다
-  // stack 빈 배열을 선언한다.
+const balancedBrackets = function (str) {
   let opBracket = ["(", "[", "{"];
   let clBracket = [")", "]", "}"];
   let stack = [];
-  let remains = "";
 
-  // 입력으로 받은 str에서 ),],} 중 하나가 나올 떄까지 Stck에 넣는다.
+  if (str.length === 1) return false;
 
   for (let i = 0; i < str.length; i++) {
     if (opBracket.includes(str[i])) stack.push(str[i]);
-    else if (stack.length !== 0) {
-      remains = str.slice(i);
-      break;
-    } else return false;
-  }
-
-  // stack의 길이가 0이 될 때까지 while문을 돌린다.
-  while (stack.length !== 0) {
-    nextB = remains[0]; // stack에 넣은 문자의 바로 다음 문자
-    openB = stack[stack.length - 1]; // stack의 제윌 위의 문자
-
-    // 만약 다음 문자들이 한  쌍이라면
-    if (clBracket.indexOf(nextB) === opBracket.indexOf(openB)) {
-      stack.pop(); // stack에서 제거한다.
-      remains = remains.slice(1); // ramains에서 첫 번째 문자를 제거한다.
-      if (!stack.length) {
-        // 만약 stack이 비었다면  남아있는 문자에서 stack을 채운다.
-        for (let i = 0; i < remains.length; i++) {
-          if (opBracket.includes(remains[i])) stack.push(remains[i]);
-          else if (stack.length === 0) return false;
-          else break;
-        }
-      }
+    else if (clBracket.includes(str[i])) {
+      let opB = stack.pop();
+      let clB = str[i];
+      if (
+        opB === undefined ||
+        opBracket.indexOf(opB) !== clBracket.indexOf(clB)
+      )
+        return false;
     }
-    // 만얃 str의 다음 문자가 [,{,( 중에 있으면 (Openbracket 문자이면) stack에 추가한다.
-    else if (opBracket.includes(nextB)) stack.push(nextB);
-    // 위의 두 경우가 아니면 return false
-    else return false;
   }
-
   return true;
 };
 
 // ! tests
-let output = balancedBrackets4("())()(()");
+let output = balancedBrackets("())()(()");
 console.log(output); // --> false
 
-let output2 = balancedBrackets4("[({})]");
+let output2 = balancedBrackets("[({})]");
 console.log(output2); // --> true
 
-let output3 = balancedBrackets4("[(]{)}");
+let output3 = balancedBrackets("[(]{)}");
 console.log(output3); // --> false
 
-let output4 = balancedBrackets4("(((((((((())))))))))");
+let output4 = balancedBrackets("(((((((((())))))))))");
 console.log(output4); // --> true
+
+let output5 = balancedBrackets(")");
+console.log(output5); // --> false
+
+let output6 = balancedBrackets("(");
+console.log(output6); // --> false
+
+let output7 = balancedBrackets("()()");
+console.log(output7); // -> treu
+
+let output8 = balancedBrackets("()");
+console.log(output8); //-> true
+
+let output9 = balancedBrackets(")(");
+console.log(output9); //-> false
+
+// //! 4 번째 - string으로 진행 해보기 + getOpenBracket 빼보기
+// const balancedBrackets = function (str) {
+//   // TODO Data Type Stack을 이용하자!
+//   let strtoArr = str.split(""); // str을 배열로 변환
+
+//   const getOpenBracket = (arr) => {
+//     // stack에 open bracket을 넣는 method . stack에 들어갈 문자를 제외한 문자들을 배열로 return 한다.
+//     for (let i = 0; i < arr.length; i++) {
+//       if (opBracket.includes(arr[i])) stack.push(arr[i]);
+//       else return stack.length === 0 ? false : arr.slice(i); // 만약 stack이  비어있다면 return false
+//     }
+//   };
+
+//   // open 문자와 close 문자를 각 각 배열로 만든다
+//   // stack 빈 배열을 선언한다.
+//   let opBracket = ["(", "[", "{"];
+//   let clBracket = [")", "]", "}"];
+//   let stack = [];
+
+//   // 입력으로 받은 str에서 ),],} 중 하나가 나올 떄까지 Stck에 넣는다. -> getOpenBracket(str)
+//   let remains = getOpenBracket(strtoArr); // stack에 넣은 나머지 문자들을 remain 변수에 배열 자료형으로  초기화
+
+//   // stack의 길이가 0이 될 때까지 while문을 돌린다.
+//   while (stack.length !== 0) {
+//     nextB = remains[0]; // stack에 넣은 문자의 바로 다음 문자
+//     openB = stack[stack.length - 1]; // stack의 제윌 위의 문자
+
+//     // 만약 다음 문자들이 한  쌍이라면
+//     if (clBracket.indexOf(nextB) === opBracket.indexOf(openB)) {
+//       stack.pop(); // stack에서 제거한다.
+//       remains.shift(); // ramains에서 제거한다.
+//       if (!stack.length) {
+//         // 만약 stack이 비었다면  남아있는 문자에서 stack을 채운다.
+//         if (getOpenBracket(remains) === false) return false; // 결과가   false면 return false
+//       }
+//     }
+//     // 만얃 str의 다음 문자가 [,{,( 중에 있으면 (Openbracket 문자이면) stack에 추가한다.
+//     else if (opBracket.includes(nextB)) stack.push(nextB);
+//     // 위의 두 경우가 아니면 return false
+//     else return false;
+//   }
+
+//   return true;
+// };
--- JAVASCRIPT/Toy-Problems/18_getItemFromTwoSortedArray.js
+++ JAVASCRIPT/Toy-Problems/18_getItemFromTwoSortedArray.js
@@ -10,7 +10,8 @@ number 타입의 0이상의 정수 k
 어떤 배열 arr의 k번째 요소는 arr[k-1]을 의미.
 */
 // k번 째로 큰 것을 찾는 문제
-const getItemFromTwoSortedArrays = function (arr1, arr2, k) {
+//! 1번 풀이 O(n)
+const getItemFromTwoSortedArrays1 = function (arr1, arr2, k) {
   // Todo
   // 두 배열은 오름차순으로 정렬되어 있다. k번 째 요소는 k번째로 큰 요소를 의미한다.
   // 두 배열에서 크기 순으로 한 개씩 꺼내서 비교를 하고 count를 증가시켜 count가 k번 째 인 경우 그 값을 return한다.
@@ -59,16 +60,104 @@ const getItemFromTwoSortedArrays = function (arr1, arr2, k) {
   }
   return result;
 };
+//! 2번 풀이 B-tree응용 O(n)
+// 첫 실행
+// arr1[0] 과 arr2[0]중 작은 것을 start node로 한다. (arr1[0]<=arr2[0])
+// com2애는 위의 결과에서 더 큰 것을 넣는다. com1=arr1[2]
+// com1에는 start node의 다음 인자를 넣는다. (arr1[1]을 넣는다)
+// if com1<=com2 이면 node를 com1으로 바꾼다. (더 작은 것을 node로 한다.)
+// com1 에 com1다음의 인자를 넣는다 count ++
+// else node를 com2로 바꾼다.  count ++
+// com2에 com2 다음 인자를 넣는다.
+// count 와 k가 같다면 return node
 
-const bSearch = (arr) => {};
+const getItemFromTwoSortedArrays2 = function (arr1, arr2, k) {
+  let count = 0;
+  let node = 0;
 
+  if (k <= (arr1.length + arr2.length) / 2) {
+    let [comp1, comp2] = [arr1.shift(), arr2.shift()];
+    while (count !== k) {
+      if (comp1 <= comp2) {
+        node = comp1;
+        count++;
+        comp1 = arr1.shift();
+      } else if (comp2 <= comp1) {
+        node = comp2;
+        count++;
+        comp2 = arr2.shift();
+      }
+    }
+  } else {
+    const len_k = arr1.length + arr2.length - k + 1; // 뒤에서 len_k번 쨰로 큰 것을 구하기 위해
+    let [comp1, comp2] = [arr1.pop(), arr2.pop()];
+    while (count !== len_k) {
+      if (comp1 <= comp2) {
+        node = comp2;
+        count++;
+        comp2 = arr1.pop();
+      } else if (comp2 <= comp1) {
+        node = comp1;
+        count++;
+        comp1 = arr2.pop();
+      }
+    }
+  }
+
+  return node;
+};
+
+const getItemFromTwoSortedArrays1 = function (arr1, arr2, k) {
+  //Todo B-Search
+
+  // arr1 과 arr2를 대소비교하여 더 작은 배열을 기준으로 한다.
+  // 더 작은 배열의 중간 값이 몇 번째인지 구한다.
+  // 만약 k보다 작으면 mid+1범위를 좁힌다.
+  // 크다면 mid-1로 범위를 좁힌다.
+  // 반복을 한다.
+
+  let mid = 0;
+
+  if (arr1.length <= arr2.length) {
+    mid = Math.floor(arr1.length / 2);
+    getCount(arr2, num, arr1.length + arr2.length);
+  }
+
+  getCount();
+};
+//? 매개변수 num이 몇 번째 숫자인지 얻어내는 메서드
+const getCount = (arr, num, len) => {
+  // num의 arr의 마지막 인자부터 시작해서 대소비교
+  let count = 0;
+  for (let i = 0; i < arr.length; i++)
+    if (arr[len - 1 - i] >= num) count++;
+    else break;
+  return len - count;
+};
 //! test
-let arr1 = [1, 4, 8, 10];
-let arr2 = [2, 3, 5, 9];
-let result = getItemFromTwoSortedArrays(arr1, arr2, 6);
-console.log(result); // --> 8
+// let arr1 = [1, 4, 8, 10];
+// let arr2 = [2, 3, 5, 9];
+// let result = getItemFromTwoSortedArrays(arr1, arr2, 6);
+// console.log(result); // --> 8
 
-arr1 = [1, 1, 2, 10];
-arr2 = [3, 3];
-result = getItemFromTwoSortedArrays(arr1, arr2, 4);
-console.log(result); // --> 3
+// arr1 = [1, 1, 2, 10];
+// arr2 = [3, 3];
+// result = getItemFromTwoSortedArrays(arr1, arr2, 4);
+// console.log(result); // --> 3
+
+let arr3 = [];
+let arr4 = [];
+let i = 0;
+let j = 1;
+while (i === 1000000) {
+  arr3.push(i + 2);
+  i++;
+  arr4.push(j + 1);
+  j++;
+  arr3.push(j + 4);
+  j++;
+  arr4.push(i + 3);
+  i++;
+}
+result = getItemFromTwoSortedArrays(arr3, arr4, 1000000);
+console.log(result);
