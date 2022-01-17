68f0feaf36667e8c52d9abdf32f7764026754a4a
sora9z <sssssssorasss@gmail.com>
2022-01-17 22:35:50 +0900

    Toy 18 - Not yet

--- JAVASCRIPT/Toy-Problems/18_getItemFromTwoSortedArray.js
+++ JAVASCRIPT/Toy-Problems/18_getItemFromTwoSortedArray.js
@@ -60,97 +60,193 @@ const getItemFromTwoSortedArrays1 = function (arr1, arr2, k) {
   }
   return result;
 };
-//! 2번 풀이 B-tree응용 O(n)
-// 첫 실행
-// arr1[0] 과 arr2[0]중 작은 것을 start node로 한다. (arr1[0]<=arr2[0])
-// com2애는 위의 결과에서 더 큰 것을 넣는다. com1=arr1[2]
-// com1에는 start node의 다음 인자를 넣는다. (arr1[1]을 넣는다)
-// if com1<=com2 이면 node를 com1으로 바꾼다. (더 작은 것을 node로 한다.)
-// com1 에 com1다음의 인자를 넣는다 count ++
-// else node를 com2로 바꾼다.  count ++
-// com2에 com2 다음 인자를 넣는다.
-// count 와 k가 같다면 return node
 
-const getItemFromTwoSortedArrays2 = function (arr1, arr2, k) {
-  let count = 0;
-  let node = 0;
+//! 두  번 풀이..
 
-  if (k <= (arr1.length + arr2.length) / 2) {
-    let [comp1, comp2] = [arr1.shift(), arr2.shift()];
-    while (count !== k) {
-      if (comp1 <= comp2) {
-        node = comp1;
-        count++;
-        comp1 = arr1.shift();
-      } else if (comp2 <= comp1) {
-        node = comp2;
-        count++;
-        comp2 = arr2.shift();
-      }
-    }
-  } else {
-    const len_k = arr1.length + arr2.length - k + 1; // 뒤에서 len_k번 쨰로 큰 것을 구하기 위해
-    let [comp1, comp2] = [arr1.pop(), arr2.pop()];
-    while (count !== len_k) {
-      if (comp1 <= comp2) {
-        node = comp2;
-        count++;
-        comp2 = arr1.pop();
-      } else if (comp2 <= comp1) {
-        node = comp1;
-        count++;
-        comp1 = arr2.pop();
-      }
-    }
-  }
-
-  return node;
-};
-
-const getItemFromTwoSortedArrays1 = function (arr1, arr2, k) {
+const getItemFromTwoSortedArrays = function (arr1, arr2, k) {
   //Todo B-Search
 
-  // arr1 과 arr2를 대소비교하여 더 작은 배열을 기준으로 한다.
-  // 더 작은 배열의 중간 값이 몇 번째인지 구한다.
-  // 만약 k보다 작으면 mid+1범위를 좁힌다.
-  // 크다면 mid-1로 범위를 좁힌다.
-  // 반복을 한다.
+  let obj1 = {
+    arr: arr1,
+    range: Math.floor(k / 2),
+    step: Math.floor(k / 4),
+    pointer: Math.floor(k / 4) - 1,
+  };
 
-  //!
+  let obj2 = {
+    arr: arr2,
+    range: Math.floor(k / 2),
+    step: Math.floor(k / 4),
+    pointer: Math.floor(k / 4) - 1,
+  };
 
-  let [head, mid, tail] = [0, 0, 0];
-  let [len1, len2] = [Math.floor(arr1.length / 2), Math.floor(arr2.length / 2)];
-  mid = len1 >= len2 ? len1 : len2;
+  const setNewObjectRange = (obj1, obj2) => {
+    // if range ===1 인 경우
 
-  while (head > tail) {
-    //mid가 몇  번째인지 구한다.
+    // k , step을 차례로 갱신한다.
+    // k가 0이면 return arr[pointer]
+    // return 0
+    if (obj1.range === 1) {
+      k -= obj1.step;
+      if (k === 0) return obj1.arr[obj1.pointer];
+      obj1.pointer += obj1.step; // pointer에 step을 미리 더한다. (다음 step이 0이 되어버리므로)
+      obj1.range -= obj1.step; // 아래의 것들은 모두 버림
+      obj1.step = Math.floor(obj1.range / 2); // 다음 단계 step으로 update
+      return 0;
+    } else if (obj1.range === 0) {
+      // else if range ===0인 경우 (더이상 진행 할 step이 없으므로 obj2에서 나눠준다 )
+      // obj2의 step이 변경되었으니 pointer수정
+      // obj1의 setp만 수정한다.
+      // return 0
+      if (obj2.range === 0)
+        // obj2 range도 0이면
+        return obj1.arr[obj1.pointer];
+      else if (obj2.range === 1) {
+        obj1.range = obj2.range;
+        obj2.range -= obj1.range;
+        obj1.step = obj1.range === 1 ? 1 : Math.floor(obj1.range / 2);
+        obj2.pointer +=
+          obj2.pointer === 0 || obj2.pointer === obj2.arr.length - 1
+            ? 0
+            : obj2.step;
+        return 0;
+      } else {
+        obj1.range = Math.floor(obj2.range / 2);
+        obj1.step = obj1.range === 1 ? 1 : Math.floor(obj1.range / 2);
+
+        obj2.range -= obj1.range;
+        obj2.pointer +=
+          obj2.pointer === 0 || obj2.pointer === obj2.arr.length - 1
+            ? 0
+            : obj2.step;
+        return 0;
+      }
+    } else {
+      // k,range,step,을 갱신한다.
+      // k가 0이면 return arr[pointer]
+
+      k -= obj1.step;
+      if (k === 0) return obj1.arr[obj1.pointer];
+      obj1.range -= obj1.step;
+      obj1.step = obj1.range === 1 ? 1 : Math.floor(obj1.range / 2);
+      obj1.pointer += obj1.step;
+      return 0;
+    }
+  };
+
+  while (k > 0) {
+    if (obj1.arr[obj1.pointer] <= obj2.arr[obj2.pointer]) {
+      let find = setNewObjectRange(obj1, obj2);
+      if (find !== 0) return find;
+    } else {
+      let find = setNewObjectRange(obj2, obj1);
+      if (find !== 0) return find;
+    }
   }
+  return 0;
 };
 
-//test;
+//! test;
 // let arr1 = [1, 4, 8, 10];
 // let arr2 = [2, 3, 5, 9];
 // let result = getItemFromTwoSortedArrays(arr1, arr2, 6);
 // console.log(result); // --> 8
 
-// arr1 = [1, 1, 2, 10];
-// arr2 = [3, 3];
-// result = getItemFromTwoSortedArrays(arr1, arr2, 4);
+// let arr3 = [1, 1, 2, 10];
+// let arr4 = [3, 3];
+// result = getItemFromTwoSortedArrays(arr3, arr4, 4);
 // console.log(result); // --> 3
 
-let arr3 = [];
-let arr4 = [];
-let i = 0;
-let j = 1;
-while (i === 1000000) {
-  arr3.push(i + 2);
-  i++;
-  arr4.push(j + 1);
-  j++;
-  arr3.push(j + 4);
-  j++;
-  arr4.push(i + 3);
-  i++;
-}
-result = getItemFromTwoSortedArrays(arr3, arr4, 1000000);
+// const arr5 = [1, 1, 2, 10];
+// const arr6 = [2, 3, 7, 12];
+// result = getItemFromTwoSortedArrays(arr5, arr6, 7);
+// console.log(result);
+
+// let ar = Array.from({ length: 30 }, () => Math.floor(Math.random() * 30));
+// let ar2 = Array.from({ length: 40 }, () => Math.floor(Math.random() * 40));
+// console.log(ar);
+// console.log(ar2);
+
+let ar = [
+  10, 24, 17, 26, 0, 0, 12, 10, 10, 8, 24, 15, 23, 26, 20, 4, 20, 14, 0, 21, 11,
+  12, 22, 18, 2, 7, 8, 23, 28, 19,
+].sort((a, b) => a - b);
+let ar2 = [
+  30, 4, 4, 5, 10, 6, 20, 14, 1, 21, 12, 27, 19, 1, 7, 11, 33, 2, 32, 8, 19, 33,
+  17, 14, 20, 21, 38, 18, 5, 1, 18, 37, 23, 12, 9, 19, 11, 6, 34, 33,
+].sort((a, b) => a - b);
+
+console.log(ar.concat(ar2).sort((a, b) => a - b));
+console.log("shuld", ar.concat(ar2).sort((a, b) => a - b)[25]);
+
+let result = getItemFromTwoSortedArrays(ar, ar2, 24);
 console.log(result);
+
+// const offset = parseInt(Math.random() * 1000) + 1;
+// const k = size - offset;
+// result = getItemFromTwoSortedArrays(arr1, arr2, k);
+// console.log(result);
+
+// let arr7 = [];
+// let arr8 = [];
+// let i = 0;
+// let j = 1;
+// while (i <= 1000000) {
+//   arr7.push(i + 2);
+//   i++;
+//   arr8.push(j + 1);
+//   j++;
+//   arr7.push(j + 4);
+//   j++;
+//   arr8.push(i + 3);
+//   i++;
+// }
+// console.log(arr7);
+// console.log(arr8);
+// result = getItemFromTwoSortedArrays(arr7, arr8, 1000000);
+// console.log(result);
+
+///
+
+// let obj1 = {
+//   arr: arr1,
+//   range: Math.floor(k / 2),
+//   step: Math.floor(k / 4),
+//   pointer: Math.floor(k / 4) - 1,
+// };
+
+// let obj2 = {
+//   arr: arr2,
+//   range: Math.floor(k / 2),
+//   step: Math.floor(k / 4),
+//   pointer: Math.floor(k / 4) - 1,
+// };
+
+// const setNewObjectRange = (obj1, obj2) => {
+//   // 만약 range가 0이면 obj2에서 range의 반을 넘겨준다.
+//   if (obj1.range === 0) {
+//     // arr2 의 range의 반을 넘겨준다.
+//     obj1.range = Math.floor(obj2.range / 2);
+//     k -= obj1.step;
+//     if (k === 0) return obj1.arr[obj1.pointer];
+//     obj1.step = Math.floor(obj1.range / 2);
+//     obj1.pointer += obj1.step;
+//     // range2 재정의
+//     obj2.range -= obj1.range; // 그만큼 range2 감소
+//     obj2.step = Math.floor(obj2.range / 2);
+//     obj2.pointer -= obj2.step;
+//     return 0;
+//   }
+//   k -= obj1.step;
+//   if (k === 0) return obj1.arr[obj1.pointer];
+//   obj1.range -= obj1.step;
+//   if (obj1.range === 1) obj1.step = 1;
+//   else obj1.step = Math.floor(obj1.range / 2);
+//   obj1.pointer += obj1.step;
+//   return 0;
+// };
+
+// obj1 ={arr, step, pointer}
+// obj2={arrstep,pointer}
+// step : 한 번에 이동할 수 있는 step
+// pointer : 현재 진행 할 배열의 index
